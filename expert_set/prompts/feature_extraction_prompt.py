90f57f7a217fd7dfeb18e35ac39c305b8ec8af68
dhc <dariohcvg@gmail.com>
2025-06-23 19:37:36 -0400

    fix(expert_set): feature extraction prompts tuning -- misc fixes

--- expert_set/prompts/feature_extraction_prompt.py
+++ expert_set/prompts/feature_extraction_prompt.py
@@ -1,8 +1,12 @@
+import json
 from typing import List
 
+from board.board import ThesisKnowledgeModel
+from ..models import PaperFeatureExtraction
+
 def build_feature_extraction_prompt(
-    expert_name: str,
     expert_description: str,
+    paper_description: str,
     document_title: str,
     document_authors: List[str],
     chunk_content: str,
@@ -12,19 +16,45 @@ def build_feature_extraction_prompt(
     features_list = "\n".join([f"- {feature}" for feature in features_to_extract])
     authors_str = ", ".join(document_authors)
     
-    return f"""You are {expert_name}, {expert_description}.
+    return f"""
+You are an expert, part of a group who is tasked with examining a set of research papers and extracting features
+from it, in order to compose a state of the art section for a specific target research paper, the state of the art 
+section is the section of a research paper that explores similar investigations on the subject by other 
+researchers, and compares them with their own work. The features should relate to the research paper, exemplifying
+the ways in which the same subject was addressed (i.e. if the paper you're reading solves a problem stated in the
+paper you're composing the state of the art section for in a different way, or shows a result that is similar
+to the ones shown in your target research paper). The objective is to create a state of the art TABLE, which
+has as rows the documents, and as columns, features, with a short description of why they were deemed detected
+in a given research paper
 
-Your task is to extract specific features from the given document chunk. You must provide a value for each feature listed below.
+This is a description of your target research paper
 
-FEATURES TO EXTRACT:
+### DESCRIPTION ###
+{paper_description}
+### END ###
+
+As such you're specialized in a specific domain which relates with the target research paper, this is your description:
+### DESCRIPTION ###
+{expert_description}
+### END ###
+
+You are reading the documents chunk by chunk, this is one of the chunks for a given research paper.
+
+Your task is to extract specific features from the given document chunk. You must provide a value for each feature listed below, i.e. a short description of how it was explored, implemented, demonstrated, etc. in the document,
+whatever is appropriate given the description of the feature
+
+### FEATURES TO EXTRACT ###
 {features_list}
+### END ###
 
-DOCUMENT INFORMATION:
+### DOCUMENT INFORMATION ###
 - Title: {document_title}
 - Authors: {authors_str}
+### END ###
 
-DOCUMENT CHUNK:
+### DOCUMENT CHUNK ###
 {chunk_content}
+### END ###
 
 INSTRUCTIONS:
 1. For each feature listed above, extract the most relevant information from the chunk
@@ -32,4 +62,7 @@ INSTRUCTIONS:
 3. Provide concise but informative values
 4. Focus on factual information from the text
 
-Respond as a JSON object with feature names as keys and extracted values as the response."""
+Respond as a JSON object with feature names as keys and extracted values as the response, as
+per the following schema
+{json.dumps(PaperFeatureExtraction.model_json_schema(), indent=2)}
+"""
